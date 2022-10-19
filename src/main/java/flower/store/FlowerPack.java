a3d44a2b7c01c3999c9c4f0174f2fd6a7db0ccdc
misha <mykhailo.humeniuk@ucu.edu.ua>
2022-10-20 02:20:46 +0300

    Tests added

--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -6,6 +6,13 @@ import lombok.Getter;
 public class FlowerPack {
     private Flower flower;
     private int quantity;
+
+
+    public FlowerPack(Flower flower, int quantity) {
+        this.flower = flower;
+        this.quantity = quantity;
+    }
+
     public double get_price() {
         return quantity * flower.getPrice();
     }
